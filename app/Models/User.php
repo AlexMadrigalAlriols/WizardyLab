3445dabfd21c68074c3c7d896e434f3c69f8c6ef
AlexMadrigalAlriols <alex.madrigal.alriols25005@gmail.com>
2024-06-15 10:41:20 +0200

    add portaL_id to users

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,6 +28,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
+        'portal_id',
         'name',
         'email',
         'password',
